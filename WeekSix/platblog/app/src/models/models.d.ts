541c7c53e402c86a37eda2a51ba5887462c235aa
Ethan Fairweather <Ethan-Fairweather@Ethans-MacBook-Pro.local>
2015-11-16 12:50:08 -0600

    last class commit of source folder

---
+++ WeekSix/platblog/app/src/models/models.d.ts
@@ -0,0 +1,13 @@
+declare module models {
+	interface IPost {
+		author: string;
+		content: string;
+		timeStampCreate: string;
+		objectId: string;
+		timeX: string;
+		title: string;
+		timeStampUpdate: string;
+	}
+	
+	interface 
+}
